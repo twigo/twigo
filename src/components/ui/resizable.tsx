e799a39337c5f2b88d809cad1cf7ba1992eb21db
Serhii Mazurok <serhii.mazurok@segla.io>
2026-06-07 02:00:25 +0200

    style: proper resize handle (wide grab zone, centered line, reliable hover/drag highlight)

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,4 +1,3 @@
-import { GripVerticalIcon } from "lucide-react";
 import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
@@ -24,26 +23,20 @@ function ResizablePanel({ ...props }: ResizablePrimitive.PanelProps) {
 }
 
 function ResizableHandle({
-  withHandle,
   className,
   ...props
-}: ResizablePrimitive.SeparatorProps & {
-  withHandle?: boolean;
-}) {
+}: ResizablePrimitive.SeparatorProps) {
   return (
     <ResizablePrimitive.Separator
       data-slot="resizable-handle"
       className={cn(
-        "relative flex w-px items-center justify-center bg-border transition-colors hover:bg-brand data-[resize-handle-active]:bg-brand after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 focus-visible:outline-hidden aria-[orientation=horizontal]:h-px aria-[orientation=horizontal]:w-full aria-[orientation=horizontal]:after:left-0 aria-[orientation=horizontal]:after:h-1 aria-[orientation=horizontal]:after:w-full aria-[orientation=horizontal]:after:translate-x-0 aria-[orientation=horizontal]:after:-translate-y-1/2 [&[aria-orientation=horizontal]>div]:rotate-90",
+        // wide, transparent grab zone with a centered 1px line
+        "group relative flex w-1.5 items-center justify-center bg-transparent outline-none focus-visible:ring-1 focus-visible:ring-ring aria-[orientation=horizontal]:h-1.5 aria-[orientation=horizontal]:w-full",
         className,
       )}
       {...props}
     >
-      {withHandle && (
-        <div className="z-10 flex h-4 w-3 items-center justify-center rounded-xs border bg-border">
-          <GripVerticalIcon className="size-2.5" />
-        </div>
-      )}
+      <div className="h-full w-px bg-border transition-colors group-hover:bg-brand group-aria-[orientation=horizontal]:h-px group-aria-[orientation=horizontal]:w-full" />
     </ResizablePrimitive.Separator>
   );
 }
